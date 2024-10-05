fc35651b6c6ba4102254eb880c8edf838a2e7015
JalalHoneycomb <130956118+JalalHoneycomb@users.noreply.github.com>
2024-10-05 21:56:42 +0530

    first commit

---
+++ TypeCasting.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class TypeCasting{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        float number = 35.23f;
+        int number2 = (int) number;
+        char ch = 'a';
+        int number3 = ch;
+        System.out.println(number2);
+        System.out.println(number3);
+
+    }
+}
